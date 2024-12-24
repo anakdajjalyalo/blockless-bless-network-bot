9828498c9b7be7a5ee8e86ce938631be5bf6777d
anakdajjalyalo <124543274+anakdajjalyalo@users.noreply.github.com>
2024-12-24 10:13:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,12 @@
 module.exports = [
     {
-        usertoken: 'usertoken1',
+        usertoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzY1MjU3YjMwMjc2NTlmNjNhN2M0YmMiLCJwdWJsaWNBZGRyZXNzIjoiOFBCMVZpczNtZ3RpZHlOMWNtZFc5cE53S2NKUXc3ODJ4OHJwbkhGRW9pemYiLCJ3YWxsZXRUeXBlIjoic29sYW5hIiwiaWF0IjoxNzM0OTExMjY3LCJleHAiOjE3NjY0Njg4Njd9.oeM5W2HeUOvhz3DncPJujIUuco9_rnMaOiNykG_vCQ4',
         nodes: [
-            { nodeId: 'nodeid1', hardwareId: 'hardwareid1', proxy: 'proxy1' },
+            { nodeId: '12D3KooWPtAzWb9zeYT5mwfgELg1oq9jRHWGsfBJobmWKDf5JSbD', hardwareId: '2a56f44f0a76787ebf6c056777249bb9ae5f1d540256bb4bd76d4bf5615a090b', proxy: 'proxy1' },
             { nodeId: 'nodeid2', hardwareId: 'hardwareid2', proxy: 'proxy2' },
             { nodeId: 'nodeid3', hardwareId: 'hardwareid3', proxy: 'proxy3' },
             { nodeId: 'nodeid4', hardwareId: 'hardwareid4', proxy: 'proxy4' },
             { nodeId: 'nodeid5', hardwareId: 'hardwareid5', proxy: 'proxy5' }
-        ]
-    },
-    {
-        usertoken: 'usertoken2',
-        nodes: [
-            { nodeId: 'nodeid6', hardwareId: 'hardwareid6', proxy: 'proxy6' },
-            { nodeId: 'nodeid7', hardwareId: 'hardwareid7', proxy: 'proxy7' },
-            { nodeId: 'nodeid8', hardwareId: 'hardwareid8', proxy: 'proxy8' },
-            { nodeId: 'nodeid9', hardwareId: 'hardwareid9', proxy: 'proxy9' },
-            { nodeId: 'nodeid10', hardwareId: 'hardwareid10', proxy: 'proxy10' }
         ]
     }
     // Add more usertokens as needed
